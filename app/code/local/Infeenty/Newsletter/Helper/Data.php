639b3fc7f150ecafd25d32629f51b073e490fca4
vipankumar1987 <vipankumar28@ymail.com>
2014-05-08 19:34:11 +0530

    removed old code
    removed old code

--- app/code/local/Infeenty/Newsletter/Helper/Data.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class Infeenty_Newsletter_Helper_Data extends Mage_Core_Helper_Abstract
-{
-}
--- app/code/local/Infeenty/Newsletter/Model/Resource/Setup.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class Infeenty_Newsletter_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup
-{
-}
