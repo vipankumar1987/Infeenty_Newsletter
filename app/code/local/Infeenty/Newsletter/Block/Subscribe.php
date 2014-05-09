38412eabcc591176ef850f30030cb005f4b14127
vipankumar1987 <vipankumar28@ymail.com>
2014-05-09 19:17:29 +0530

    Newsletter widget effected by admin settings (Enable Lightbox/ Enable onStartup)
    Newsletter widget effected by admin settings (Enable Lightbox/ Enable
    onStartup)

--- app/code/local/Infeenty/Newsletter/Block/Subscribe.php
+++ app/code/local/Infeenty/Newsletter/Block/Subscribe.php
@@ -22,7 +22,19 @@ class Infeenty_Newsletter_Block_Subscribe extends Mage_Newsletter_Block_Subscrib
 		);
 		return $fields;
 	}
+	private function getConfig($path){
+	
+		return Mage::getStoreConfig(
+				$path, 
+				Mage::app()->getStore()->getId()
+			);
+	}
 	protected function isLightBoxEnabled(){
-		return true;
+
+		return (bool)$this->getConfig('infeenty_newsletter/general/enable_lightbox');
+	}
+	protected function isOnload(){
+		return (bool)$this->getConfig('infeenty_newsletter/general/onstartup');
+
 	}
 }
