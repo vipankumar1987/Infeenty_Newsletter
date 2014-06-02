b6539c3c2d8b8077412d654e2b9c45edca16e643
vipan kumar <vipan.webdeveloper@gmail.com>
2014-06-02 18:31:36 +0530

    Updated License

--- app/code/local/Infeenty/Newsletter/Block/Recaptcha/Red.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Infeenty_Newsletter_Block_Recaptcha_Red extends Mage_Adminhtml_Block_System_Config_Form_Field
-{
-    protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
-    {
-		return "<img src='https://developers.google.com/recaptcha/images/reCAPTCHA_Sample_Red.png'/>";
-	}
-}
